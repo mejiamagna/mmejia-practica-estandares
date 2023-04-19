85382d68da1eca4816eb935036cb485f0625afc6
mejiamagna <51210347+mejiamagna@users.noreply.github.com>
2023-04-18 23:10:07 -0500

    estilo boton

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('Hello!');
+
+const botonEstilo2 = document.getElementById('estilo2');
+
+botonEstilo2.addEventListener('click', () => {
+  document.querySelector('link[href="styles.css"]').href = 'styles2.css';
+});
